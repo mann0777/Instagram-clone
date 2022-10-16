3614286ce10a720c20f437b99ddf54660225297a
mann0777 <mannamloh77@gmail.com>
2022-10-16 11:26:20 +0530

    CreateContext and form with basic css

--- src/Global/Context.js
+++ src/Global/Context.js
@@ -3,8 +3,11 @@ export const ContextProvider = createContext();
 
 const Context = (props) => {
     const [model, setModel] = useState(false);
+    const openModel=()=>{
+        setModel(true);
+    };
   return (
-    <ContextProvider.Provider value={{model}}>
+    <ContextProvider.Provider value={{model, openModel}}>
     {props.children}
     </ContextProvider.Provider>
 
